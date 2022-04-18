c1370f8a82aba8960e894a604241501d98150e6d
Hudson <hudsonmachado2012@gmail.com>
2022-04-18 07:43:11 -0400

    update label

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
-          SQL FORMATTER TOLL
+          SQL FORMAT TOOL
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
