4faeafbe46d242386634fd93478dfed59a4f2e36
Hudson <hudsonmachado2012@gmail.com>
2022-07-07 13:46:26 -0400

    update layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,7 +3,7 @@
     <div class="" style="display: flex; flex-direction: column; justify-content: center; align-items: center">
       <span class="q-pa-md" style="font-size: 25px">Online SQL Editor</span>
       <p style="font-size: 16px; text-align: justify;" class="q-pa-md">
-        Editar SQL documentos online a partir de qualquer dispositivo com um navegador moderno como o Chrome e Firefox
+        Editar SQL online a partir de qualquer dispositivo com um navegador moderno como o Chrome e Firefox
         <br>
         Este editor on-line, devido à sua natureza baseada na Web, é absolutamente portátil e multiplataforma — você só precisa de um navegador de web usual sem plugins, e não importa em qual plataforma você está usando: PC desktop ou smartphone, Windows, Linux ou macOS, Android ou iOS.
       </p>
